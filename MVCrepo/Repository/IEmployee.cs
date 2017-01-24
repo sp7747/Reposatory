f90cfa80a108b0e2b7534524dd686ad60380f10d
j.developer@meddatacarepro.com <pass1234>
2017-01-24 15:44:34 +0530

    second

---
+++ MVCrepo/Repository/IEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVCrepo.Models;
+namespace MVCrepo.Repository
+{
+    interface IEmployee
+    {
+
+
+        void InsertEmployee(EmployeeDetail Employee); // C
+
+        IEnumerable<EmployeeDetail> GetEmployees(); // R
+
+        EmployeeDetail GetEmployeeByID(int EmployeeId); // R
+
+        void UpdateEmployee(EmployeeDetail Employee); //U
+
+        void DeleteEmployee(int EmployeeId); //D
+
+        void Save();
+
+    }
+}
